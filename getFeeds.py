67212b918f86b15df083ab68dcfb93983154fac1
moldypeach <spacedreamsit@gmail.com>
2016-08-16 15:57:04 -0400

    hurried idea messes

--- getFeeds.py
+++ getFeeds.py
@@ -3,22 +3,26 @@
 import urllib3 #https://pypi.python.org/pypi/urllib3
 import shutil #https://docs.python.org/3/library/shutil.html
 import validators #https://pypi.python.org/pypi/validators
+import hashlib # For uniqueness comparisons
+import os # For file system operations
 
 class GetFeeds:
 	"""A simple class to get RSS and ATOM feeds"""
 
 	# Class variables shared by all instances
-	feedDir = "./feeds/"
+	feedBaseDir = "./feeds"
+	feedSrcDir = feedBaseDir + "/sources"
+	feedDownDir = "/downloads"
 	mtaFeed = "http://www.malware-traffic-analysis.net/blog-entries.rss"
 
 	# Instance variables unique to each instance
-	def __init__(self, feedURL=mtaFeed, Outfile="default"):
+	def __init__(self, feedURL=mtaFeed, fileOut="default"):
 		self.feedURL = feedURL
-		self.Outfile = Outfile
-		self.OutfileURL = self._buildOutfileURL()
+		self.outfile = fileOut
+		self.outPATH = self._buildOutPath()
+		self.outURL = self._buildOutURL()
 
 	def getFeedURL(self):
-		#print("\"" + self.feedURL + "\"")
 		return self.feedURL
 
 	def setFeedURL(self, url):
@@ -28,22 +32,31 @@ class GetFeeds:
 			print("ERROR: invalid URL sntax")
 
 	def getOutfile(self):
-		return self.Outfile
+		return self.outfile
 
-	def setOutfile(self, file):
-		self.Outfile = file
+	def setOutfile(self, fileOut):
+		self.outfile = fileOut
 
 	def downloadFeed(self):
 		# http://stackoverflow.com/questions/27387783/how-to-download-a-file-with-urllib3
 		http = urllib3.PoolManager()
-		with http.request('GET', self.getFeedURL(), preload_content=False) as r, open(self.getOutfileURL(), 'wb') as rssFile:
+		with http.request('GET', self.getFeedURL(), preload_content=False) as r, open(self.getOutURL(), 'wb') as rssFile:
 			shutil.copyfileobj(r, rssFile) #https://docs.python.org/3.4/library/shutil.html
 
 	def _validateFeedURL(self, url):
 		return validators.url(url)
 
-	def _buildOutfileURL(self):
-		return str(self.feedDir + self.Outfile)
+	def _buildOutPath(self):
+		return str(self.feedDir + self.outfile)
 
-	def getOutfileURL(self):
-		return self.OutfileURL
+	def _buildOutURL(self):
+		return str(self.feedDir + self.outfile)
+
+	def getOutURL(self):
+		return self.outURL
+
+	def genFileMD5(self,inFile):
+		return hashlib.md5(open(inFile, mode='rb').read()).hexdigest()
+
+	def genStrMD5(self,inStr):
+		return hashlib.md5(inStr.encode("utf-8")).hexdigest()	
