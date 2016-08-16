774664198e295f8adaa429fe2703c4eeda79336d
moldypeach <spacedreamsit@gmail.com>
2016-08-16 19:56:16 -0400

    refactored getFeeds - more work yet to do

--- getFeeds.py
+++ getFeeds.py
@@ -10,53 +10,44 @@ class GetFeeds:
 	"""A simple class to get RSS and ATOM feeds"""
 
 	# Class variables shared by all instances
-	feedBaseDir = "./feeds"
-	feedSrcDir = feedBaseDir + "/sources"
+	feedBaseDir = "./feeds/"
+	feedSrcDir = feedBaseDir + "sources/"
 	feedDownDir = "/downloads"
-	mtaFeed = "http://www.malware-traffic-analysis.net/blog-entries.rss"
 
 	# Instance variables unique to each instance
-	def __init__(self, feedURL=mtaFeed, fileOut="default"):
-		self.feedURL = feedURL
-		self.outfile = fileOut
-		self.outPATH = self._buildOutPath()
+	def __init__(self, feedURL):
+		self.feedURL = self.setFeedURL(feedURL)
+		self.outfile = self._buildOutFile()
+		self.outDir = self._buildOutDir()
 		self.outURL = self._buildOutURL()
 
 	def getFeedURL(self):
 		return self.feedURL
-
+	#Validates http(s):// URL is valid
 	def setFeedURL(self, url):
-		if self._validateFeedURL(str(url)):
-			self.feedURL = url
+		if validators.url(url):
+			return url
 		else:
 			print("ERROR: invalid URL sntax")
-
-	def getOutfile(self):
-		return self.outfile
-
-	def setOutfile(self, fileOut):
-		self.outfile = fileOut
-
+	#Download feed and write it to a file previously defined
 	def downloadFeed(self):
 		# http://stackoverflow.com/questions/27387783/how-to-download-a-file-with-urllib3
 		http = urllib3.PoolManager()
-		with http.request('GET', self.getFeedURL(), preload_content=False) as r, open(self.getOutURL(), 'wb') as rssFile:
+		with http.request('GET', self.feedURL, preload_content=False) as r, open(self.outURL, 'wb') as rssFile:
 			shutil.copyfileobj(r, rssFile) #https://docs.python.org/3.4/library/shutil.html
-
-	def _validateFeedURL(self, url):
-		return validators.url(url)
-
-	def _buildOutPath(self):
-		return str(self.feedDir + self.outfile)
-
+	#Returns last item of split URL list as the filename, e.g. file.rss
+	def _buildOutFile(self):
+		return self.feedURL.split('/').pop(-1)
+	#Returns the second item of split, after protocol, as directory name
+	def _buildOutDir(self):
+		return self.feedURL.split('.').pop(1)
+	#Returns the complete URL of file to store downloaded feed
 	def _buildOutURL(self):
-		return str(self.feedDir + self.outfile)
-
-	def getOutURL(self):
-		return self.outURL
-
+		return self.feedSrcDir + self.outfile
+	#Return MD5 hash of an entire file in binary mode
 	def genFileMD5(self,inFile):
 		return hashlib.md5(open(inFile, mode='rb').read()).hexdigest()
-
+	#Return MD5 hash of a string converted to utf-8, which returns a bytes object
 	def genStrMD5(self,inStr):
-		return hashlib.md5(inStr.encode("utf-8")).hexdigest()	
+		return hashlib.md5(inStr.encode("utf-8")).hexdigest()
+
