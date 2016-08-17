38cd2e4faca827573d9d5040aac8c6d40c80b00e
moldypeach <spacedreamsit@gmail.com>
2016-08-17 12:39:24 -0400

    found/fixed trailing newline issue with variables, but urllib3 continues to be problematic

--- getFeeds.py
+++ getFeeds.py
@@ -44,16 +44,16 @@ class GetFeeds:
 			sys.exit()
 		else:
 			for item in self.feeds:
-				print("Item ln 46: " + item)
+				#print("Item ln 46: " + "\"" + item + "\"")
 				self.outfile = self._buildOutFile(item)
-				print("outfile ln 47: " + self.outfile)
+				#print("outfile ln 47: " + "\"" + self.outfile + "\"")
 				self.outDir = self._buildOutDir(item)
-				print("outDir ln 49: " + self.outDir)
+				#print("outDir ln 49: " + "\"" + self.outDir + "\"")
 				self.feedDstDir = self.feedDstRootDir + self.outDir
-				print("feedDstDir ln 52: " + self.feedDstDir)
+				#print("feedDstDir ln 52: " + "\"" + self.feedDstDir + "\"")
 				self.outURL = self._buildOutURL()
-				print("outURL ln 54: " + self.outURL)
-				self.downloadFeed(item)
+				#print("outURL ln 54: " + "\"" + self.outURL + "\"")
+				self.downloadFeed(item.strip(' \r\n\t'))
 	#Return the current feed URL
 	def getFeedURL(self):
 		return self.feedURL
@@ -65,24 +65,29 @@ class GetFeeds:
 			print("ERROR: invalid URL sntax:\n\t" + url)
 	#Download feed and write it to a file previously defined
 	def downloadFeed(self, feedURL):
-		print("feedURL ln 67: " + feedURL)
+		#print("feedURL ln 67: " + "\"" + feedURL + "\"")
+		#print("variable feedURL type: " + type(feedURL).__name__)
 		#Create feed download directory if it doesn't already exist
 		#os.makedirs(self.feedDstRootDir, exist_ok=True)
 		os.makedirs(self.feedDstDir, exist_ok=True)
 
 		# http://stackoverflow.com/questions/27387783/how-to-download-a-file-with-urllib3
-		http = urllib3.PoolManager()
+		user_agent = {"user-agent": "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:43.0) Gecko/20100101 Firefox/43.0"}
+		http = urllib3.PoolManager(headers=user_agent)
+		
 		with http.request('GET', feedURL, preload_content=False) as r, open(self.outURL, 'wb') as rssFile:
 			shutil.copyfileobj(r, rssFile) #https://docs.python.org/3.4/library/shutil.html
 	#Returns last item of split URL list as the filename, e.g. file.rss
 	def _buildOutFile(self, feedURL):
-		return feedURL.split('/').pop(-1)
+		return feedURL.split('/').pop(-1).strip(' \n\r\t')
 	#Returns the second item of split, after protocol, as directory name
 	def _buildOutDir(self, feedURL):
-		return feedURL.split('.').pop(1)
+		tmp = feedURL.split('.').pop(1).strip(' \n\r\t')
+		return tmp.strip(' \n\r\t')
 	#Returns the complete URL of file to store downloaded feed
 	def _buildOutURL(self):
-		return self.feedDstRootDir + self.outDir + "/" + self.outfile
+		tmp = self.feedDstRootDir + self.outDir + "/" + self.outfile
+		return tmp.strip(' \n\r\t')
 	#Return MD5 hash of an entire file in binary mode
 	def genFileMD5(self,inFile):
 		return hashlib.md5(open(inFile, mode='rb').read()).hexdigest()
